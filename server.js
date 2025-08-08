0cf0de4c6efc5d9b6d95ef4a921da2f4147d1c82
Toniiy <131479362+Toniiy@users.noreply.github.com>
2025-08-08 00:08:08 -0700

    Fix closing bracket in server.js

--- server.js
+++ server.js
@@ -89,5 +89,10 @@ app.get('/health', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
+  console.log('Server is running on port ' + 
+   
+             );
+  
+  
 
+});
